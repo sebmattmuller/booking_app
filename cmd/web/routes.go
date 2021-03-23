bc4dbe9aada6d37004b15679a937c3ee130ef0db
sebastianm <sebastian.mattmuller@gmail.com>
2021-03-23 18:06:58 +0100

    first commit

---
+++ cmd/web/routes.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/sebmattmuller/booking_app/pkg/config"
+	"github.com/sebmattmuller/booking_app/pkg/handlers"
+	"net/http"
+)
+
+func routes(app *config.AppConfig) http.Handler{
+	//Pat Router
+	//mux := pat.New()
+	//mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	//mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+
+	mux := chi.NewRouter()
+
+	// Middleware
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+	mux.Use(SessionLoad)
+
+	//Routes
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+
+	return mux
+}
+
+
